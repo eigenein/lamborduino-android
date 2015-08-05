54b9addeb216759e2bb53487ace62b87e5664147
Pavel Perestoronin <eigenein@gmail.com>
2015-08-06 01:50:55 +0300

    Added icons. Integrated JoypadView.

--- app/src/main/java/ninja/eigenein/lamborduino/activities/MainActivity.java
+++ app/src/main/java/ninja/eigenein/lamborduino/activities/MainActivity.java
@@ -1,11 +1,7 @@
 package ninja.eigenein.lamborduino.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import ninja.eigenein.lamborduino.R;
 
@@ -17,22 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        final Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(final MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 }
